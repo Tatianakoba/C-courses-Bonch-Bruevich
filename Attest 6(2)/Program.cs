8aa66d94a392f90d28411d04d105fbe611799f37
Tatiana <lila-lilu@mail.ru>
2022-06-21 22:54:56 +0300

    21/06/2022(2)

---
+++ Attest 6(2)/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+//6.	Составить программу, которая из некоторой строки  уберет все пропуски.
+
+namespace Attest_6_2_
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Введите строку");
+            string text = Console.ReadLine();
+
+            for (int i = 0; i < text.Length; i++)
+            {
+
+                if (text[i] == ' ') 
+                {
+                    text = text.Remove(i, 1);
+                }
+            }
+
+            Console.WriteLine(text);
+            Console.ReadKey();
+        }
+    }
+}
